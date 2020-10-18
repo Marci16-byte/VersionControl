19d181066230d95f97a3e56df9b700555149213f
Marcell Kovács <marcell.kovacs3@stud.uni-corvinus.hu>
2020-10-18 18:38:33 +0200

    commit

--- week06/week06/Entities/RateData.cs
+++ week06/week06/Entities/RateData.cs
@@ -8,7 +8,7 @@ namespace week06.Entities
 {
     class RateData
     {
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set;  }
         public string Currency { get; set; }
         public decimal Value { get; set; }
 
