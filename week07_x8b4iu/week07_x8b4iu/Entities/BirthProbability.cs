a08425337b4533078ca01802d88b7f39cd5e4a1a
Marcell Kovács <marcell.kovacs3@stud.uni-corvinus.hu>
2020-10-25 15:26:50 +0100

    Commit

---
+++ week07_x8b4iu/week07_x8b4iu/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07_x8b4iu.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double BPobability { get; set; }
+        public double BProbability { get; internal set; }
+    }
+}
---
+++ week07_x8b4iu/week07_x8b4iu/Entities/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07_x8b4iu.Entities
+{
+    public class Person
+    {
+        public int BirtYear { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public int NbrOfChildren { get; set; }
+
+        public bool IsAlive { get; set; }
+        public int BirthYear { get; internal set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
