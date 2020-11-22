dda782a88640e61b5ea8e9059e3bbc850ccf912a
Marcell Kovács <marcell.kovacs3@stud.uni-corvinus.hu>
2020-11-22 18:25:20 +0100

    Commit1

--- week09/week09/Form1.cs
+++ week09/week09/Form1.cs
@@ -14,7 +14,49 @@ namespace week09
     {
         public Form1()
         {
+           private AccountController _controller = new AccountController();
+
+        private bool passwordCheckPassed;
+        public bool PasswordCheckPassed
+        {
+            get { return passwordCheckPassed; }
+            set
+            {
+                passwordCheckPassed = value;
+                btnRegister.Enabled = passwordCheckPassed;
+                if (passwordCheckPassed)
+                    txtPasswordConfirm.BackColor = Color.White;
+                else
+                    txtPasswordConfirm.BackColor = Color.Fuchsia;
+            }
+        }
+
+        public Form1()
+        {
             InitializeComponent();
+            PasswordCheckPassed = true;
+            dgwAccounts.DataSource = _controller.AccountManager.Accounts;
+        }
+
+        private void OnPasswordTextChanged(object sender, EventArgs e)
+        {
+            PasswordCheckPassed = txtPassword.Text.Equals(txtPasswordConfirm.Text);
+        }
+
+        private void OnRegisterClick(object sender, EventArgs e)
+        {
+            try
+            {
+                _controller.Register(
+                    txtEmail.Text,
+                    txtPassword.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
+
+
 }
