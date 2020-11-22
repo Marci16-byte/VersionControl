a3d05bd0a6cb87a5c7b93ea1c55e422bf6b83cd3
Marcell Kovács <marcell.kovacs3@stud.uni-corvinus.hu>
2020-11-22 19:11:51 +0100

    Commit UnitTest

---
+++ week09/UnitTestProject1.X8B4IU/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1.X8B4IU")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1.X8B4IU")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98dcb0ad-a5d4-4e6e-9f3c-577c6f8f8d8c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
