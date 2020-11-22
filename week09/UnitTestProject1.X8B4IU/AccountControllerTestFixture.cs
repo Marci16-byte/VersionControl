580ef002f0f74c689a3a91a6501937f2039426d3
Marcell Kovács <marcell.kovacs3@stud.uni-corvinus.hu>
2020-11-22 19:44:59 +0100

    Commit VÉg

--- week09/UnitTestProject1.X8B4IU/AccountControllerTestFixture.cs
+++ week09/UnitTestProject1.X8B4IU/AccountControllerTestFixture.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using UnitTestProject1.X8B4IU;
 using NUnit.Framework;
 using System.Activities;
-using System.UnitTestExample.Controllers;
+using UnitTestExample.Controllers;
 
 namespace UnitTestProject1.X8B4IU
 {
